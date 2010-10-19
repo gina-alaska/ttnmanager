b59bdd7a7ce2191d7b5c8e7e908236eefff6137e
Will Fisher <will@gina.alaska.edu>
2010-10-19 11:45:08 -0800

    comment out mailer for now

--- app/mailers/zone_mailer.rb
+++ app/mailers/zone_mailer.rb
@@ -1,9 +1,10 @@
 class ZoneMailer < ActionMailer::Base
   default :from => "webdev@gina.alaska.edu"
-
+=begin
   def status_email(zone)
     @zone = zone
     mail(:to => 'will@gina.alaska.edu',
          :subject => '')
   end
+=end
 end

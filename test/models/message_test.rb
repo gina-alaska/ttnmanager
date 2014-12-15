62bcb26c8686c03068f27a45abcfe79ed66bb1df
Scott Macfarlane <scott@gina.alaska.edu>
2014-12-15 11:18:15 -0900

    Add relation tests

--- test/models/message_test.rb
+++ test/models/message_test.rb
@@ -4,4 +4,7 @@ class MessageTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  should have_many(:areas_messages)
+  should have_many(:areas).through(:areas_messages)
 end

f3463b25d578c2e4dbb781db7300010ea4052f05
Scott Macfarlane <scott@gina.alaska.edu>
2014-12-15 11:42:44 -0900

    Initial pass at welcome page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,5 @@
 class WelcomeController < ApplicationController
+  def index
+    @areas = Area.all
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def login_helper
+    #if current_user.logged_in?
+      #"Logout"
+    #else
+    link_to "Login", "#"
+  end
 end

4e5ebf0e737b816ae95d8e8bde9077dffa2e2c46
Scott Macfarlane <scott@gina.alaska.edu>
2015-01-15 12:50:52 -0900

    Use correct url for requesting the map

--- app/controllers/areas_controller.rb
+++ app/controllers/areas_controller.rb
@@ -1,7 +1,7 @@
 class AreasController < ApplicationController
   before_action :set_area, only: [:show, :edit, :update, :destroy]
   authorize_resource
-  
+
   # GET /areas
   # GET /areas.json
   def index
@@ -72,7 +72,8 @@ class AreasController < ApplicationController
       width, height = determine_size(size)
       layers = %w{landsat_pan zones roads labels}.join(",")
 
-      "http://atn.proto.gina.alaska.edu/atn??VERSION=1.1.1&REQUEST=GetMap&SRS=EPSG:3338&BBOX=-93449.8805055504,2003772.41212257,389482.569861798,2337148.0788739&WIDTH=#{width}&HEIGHT=#{height}&LAYERS=#{layers}&STYLES=&EXCEPTIONS=application/vnd.ogc.se_xml&FORMAT=#{imgtype}&BGCOLOR=0xFFFFFF&TRANSPARENT=TRU"
+
+      "http://ogc.gina.alaska.edu/ttn??VERSION=1.1.1&REQUEST=GetMap&SRS=EPSG:3338&BBOX=-93449.8805055504,2003772.41212257,389482.569861798,2337148.0788739&WIDTH=#{width}&HEIGHT=#{height}&LAYERS=#{layers}&STYLES=&EXCEPTIONS=application/vnd.ogc.se_xml&FORMAT=#{imgtype}&BGCOLOR=0xFFFFFF&TRANSPARENT=TRU"
     end
 
     def determine_size(size)

8e3268bb5be1bdf0175f0a61099942870f58128f
Scott Macfarlane <scott@gina.alaska.edu>
2015-02-01 14:53:23 -0900

    Order by name for welcome#index

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,5 @@
 class WelcomeController < ApplicationController
   def index
-    @areas = Area.all
+    @areas = Area.order(:name)
   end
 end
--- app/models/area.rb
+++ app/models/area.rb
@@ -2,8 +2,6 @@ class Area < ActiveRecord::Base
   extend FriendlyId
   friendly_id :name, use: :slugged
 
-  default_scope { order('name') }
-
   has_many :areas_messages
   has_many :messages, through: :areas_messages
 

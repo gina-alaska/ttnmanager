716597287c2d472f07f7e18ecc8d7fd7be8223de
Scott Macfarlane <scott@gina.alaska.edu>
2015-05-20 13:53:17 -0800

    Add test for role description helper

--- test/helpers/users_helper_test.rb
+++ test/helpers/users_helper_test.rb
@@ -1,4 +1,11 @@
 require 'test_helper'
 
 class UsersHelperTest < ActionView::TestCase
+  include UsersHelper
+
+  test 'role help is a description list' do
+    assert_includes role_help, "Guest"
+    assert_includes role_help, "Manager"
+    assert_includes role_help, "Admin"
+  end
 end

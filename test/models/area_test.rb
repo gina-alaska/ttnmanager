73553552809278dc271143ca79d535e54f5b6857
Scott Macfarlane <scott@gina.alaska.edu>
2015-05-20 11:58:37 -0800

    Add test for getting area geometry

--- test/models/area_test.rb
+++ test/models/area_test.rb
@@ -3,4 +3,12 @@ require 'test_helper'
 class AreaTest < ActiveSupport::TestCase
   should have_many(:areas_messages)
   should have_many(:messages).through(:areas_messages)
+
+  def setup
+    @area = areas(:one)
+  end
+
+  test 'geometry should return GeoRuby' do
+    assert_instance_of GeoRuby::SimpleFeatures::MultiPolygon, @area.geometry
+  end
 end
